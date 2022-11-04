f831bfeb6349a74c94f552b94893388a6194b75e
ururu <lowlevelskill@mail.ru>
2022-11-04 15:34:42 +0300

    feat: added solution 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const {stdout} = process;
+const stream = fs.createReadStream('./01-read-file/text.txt', 'utf-8');
+let data = '';
+
+stream.on('data', (chunk)=> {
+  data += chunk;
+})
+stream.on('end', ()=> {
+  stdout.write(data);
+})
